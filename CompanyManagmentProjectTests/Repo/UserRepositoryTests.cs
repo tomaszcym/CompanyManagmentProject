1f9b252ff95d4e9712af641f57d0b1987273a17a
morrtes <tjcymerys@gmail.com>
2020-07-01 23:12:52 +0200

    pierwsze testy

---
+++ CompanyManagmentProjectTests/Repo/UserRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompanyManagmentProject.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompanyManagmentProject.Model;
+
+namespace CompanyManagmentProject.Repo.Tests
+{
+    [TestClass()]
+    public class UserRepositoryTests
+    {
+
+        private User createTestUser(string username, string password)
+        {
+            User user = new User(username, password);
+            List<User> users = UserRepository.getAll();
+            User userInDb = users.Find(u => u.username == user.username);
+            return userInDb;
+        }
+
+        [TestMethod()]
+        public void createUserTest()
+        {
+            User user = createTestUser("jackowy", "noweSilneHasło!.!%");
+            Assert.IsNotNull(user);
+        }
+
+    }
+}
